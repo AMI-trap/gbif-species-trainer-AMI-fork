7317a7602de66a640f11109f2d2597fa82cd0d16
Levan Bokeria <levan.bokeria@gmail.com>
2023-06-07 18:47:17 +0100

    added shell scripts and command text file for baskerville

---
+++ data_download/02-fetch_gbif_moth_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --account vjgo8416-amber
+#SBATCH --qos turing
+#SBATCH --time 02:00:00
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 36
+
+# Module loading
+module purge # unloads and loaded modules and resets the environment
+module load baskerville # loads the default Baskerville environment 
+module load bask-apps/live # load the stable, default application stack
+module load Miniconda3/4.10.3 # load the Conda package manager
+
+set -e # exit immediately if there is an error
+eval "$(${EBROOTMINICONDA3}/bin/conda shell.bash hook)" # initialise Conda
+
+# export CONDA_ENV_NAME="/bask/homes/r/rybf4168/.conda/envs/gbif_species_trainer"
+
+export CONDA_ENV_PATH="/bask/projects/v/vjgo8416-amber/conda_envs/gbif-species-trainer-AMI-fork"
+
+# Activate the environment
+conda activate "${CONDA_ENV_PATH}"
+
+# Execute your python programme
+
+python 02-fetch_gbif_moth_data.py \
+--write_directory /bask/projects/v/vjgo8416-amber/projects/gbif-species-trainer-AMI-fork/data_download/output_data/gbif_data_uksi_macro_moths_small_try/  \
+--dwca_file /bask/projects/v/vjgo8416-amber/data/gbif-species-trainer-AMI-fork/0001402-230530130749713.zip \
+--species_checklist bask/projects/v/vjgo8416-amber/projects/gbif-species-trainer-AMI-fork/uksi-macro-moths-small-try-keys.csv \
+--max_images_per_species 2 \
+--resume_session True 
