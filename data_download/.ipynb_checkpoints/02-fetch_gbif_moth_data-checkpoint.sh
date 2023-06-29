36ccd7f167cd3e0798801278ce5d35eff96a6a93
Levan Bokeria <levan.bokeria@gmail.com>
2023-06-29 15:05:12 +0100

    latest try

--- data_download/.ipynb_checkpoints/02-fetch_gbif_moth_data-checkpoint.sh
+++ data_download/.ipynb_checkpoints/02-fetch_gbif_moth_data-checkpoint.sh
@@ -4,7 +4,6 @@
 #SBATCH --time 02:00:00
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 36
-#SBATCH --mem=200G
 
 # Module loading
 module purge # unloads and loaded modules and resets the environment
@@ -26,7 +25,7 @@ conda activate "${CONDA_ENV_PATH}"
 
 python 02-fetch_gbif_moth_data.py \
 --write_directory /bask/projects/v/vjgo8416-amber/data/gbif-species-trainer-AMI-fork/gbif_images/small_try/  \
---dwca_file /bask/projects/v/vjgo8416-amber/data/gbif-species-trainer-AMI-fork/dwca_files/lepidoptera.zip \
---species_checklist bask/projects/v/vjgo8416-amber/projects/gbif-species-trainer-AMI-fork/uksi-macro-moths-small-try-keys.csv \
---max_images_per_species 5 \
---resume_session False 
+--dwca_file /bask/projects/v/vjgo8416-amber/data/gbif-species-trainer-AMI-fork/dwca_files/erebidae.zip \
+--species_checklist bask/projects/v/vjgo8416-amber/projects/gbif-species-trainer-AMI-fork/data_download/uksi-macro-moths-keys.csv \
+--max_images_per_species 20 \
+--resume_session False
