ae3ff6e8de3933e139b6a73461eb288019b3f77b
Levan Bokeria <levan.bokeria@gmail.com>
2023-06-29 18:19:17 +0400

    minor

--- data_download/import pandas as pd.py
+++ data_download/import pandas as pd.py
@@ -2,13 +2,40 @@ import pandas as pd
 import os
 from dwca.read import DwCAReader
 
+# filepath = '/Users/lbokeria/Downloads/'
+
+
+# dwca = DwCAReader(os.path.join(filepath, '0033583-230530130749713.zip'))
+
+# df = dwca.pd_read("occurrence.txt", parse_dates=True, on_bad_lines="skip")
+
+# print(df['acceptedTaxonKey'])
+
+# print('done')
+
+# columns = [
+#     "accepted_taxon_key",
+#     "family_name",
+#     "genus_name",
+#     "search_species_name",
+#     "gbif_species_name",
+#     "image_count",
+# ]
+
+# datacount_file = pd.DataFrame(columns=columns, dtype=object)
+
+# search_species = "phegea"
+
+# if search_species not in datacount_file["search_species_name"].tolist():
+#     print('Not here')
+
+
+#########################
+# Load an occurrence.txt file
+
 filepath = '/Users/lbokeria/Downloads/'
-
-
 dwca = DwCAReader(os.path.join(filepath, '0033583-230530130749713.zip'))
 
 df = dwca.pd_read("occurrence.txt", parse_dates=True, on_bad_lines="skip")
 
-print(df['acceptedTaxonKey'])
-
-print('done')
+print('whatever')
