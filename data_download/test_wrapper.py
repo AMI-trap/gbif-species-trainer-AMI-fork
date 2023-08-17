b93594d993a9f31e561e56f4556100243c2f3490
Levan Bokeria <levan.bokeria@gmail.com>
2023-08-17 14:07:55 +0100

    testing

---
+++ data_download/test_wrapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import sys
+
+sys.modules["fetch"] = __import__("02-fetch_gbif_moth_data_serial")
+
+from fetch import download_data
+
+print('something random')
